4cd2dac01a72e3b1449cf05185686d63084c04de
Danil <61193948+CrappyCodeMaker@users.noreply.github.com>
2021-02-09 15:52:43 +0300

    PrivatRoute

--- src/contexts/AuthContext.js
+++ src/contexts/AuthContext.js
@@ -1,5 +1,6 @@
-import React, { useContext, useEffect, useState } from 'react';
+import React, { useContext, useState, useEffect } from 'react';
 import { auth } from '../firebase';
+
 const AuthContext = React.createContext();
 
 export function useAuth() {
@@ -14,18 +15,45 @@ export function AuthProvider({ children }) {
     return auth.createUserWithEmailAndPassword(email, password);
   }
 
+  function login(email, password) {
+    return auth.signInWithEmailAndPassword(email, password);
+  }
+
+  function logout() {
+    return auth.signOut();
+  }
+
+  function resetPassword(email) {
+    return auth.sendPasswordResetEmail(email);
+  }
+
+  function updateEmail(email) {
+    return currentUser.updateEmail(email);
+  }
+
+  function updatePassword(password) {
+    return currentUser.updatePassword(password);
+  }
+
   useEffect(() => {
     const unsubscribe = auth.onAuthStateChanged((user) => {
       setCurrentUser(user);
       setLoading(false);
     });
+
     return unsubscribe;
   }, []);
 
   const value = {
     currentUser,
+    login,
     signup,
+    logout,
+    resetPassword,
+    updateEmail,
+    updatePassword,
   };
+
   return (
     <AuthContext.Provider value={value}>
       {!loading && children}
